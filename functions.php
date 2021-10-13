26e1f1d34c382890d9ab4e217927981495e5bc80
n0153529 <91226517+n0153529@users.noreply.github.com>
2021-10-13 13:09:33 +0100

    Update functions.php
    Implementation of list to output based on user data. - JOE
    --> Was working. New bug must exist as doesnt output correctly.

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // IG TRACKER CODE
 // IG TRACKER CODE
 // IG TRACKER CODE
@@ -47,13 +48,14 @@ function update_ig_entry(){
 			$datetype = $_POST['datetype'];
 			$section = $_POST['section'];
 			$typea = $_POST['typea'];
-			$typeb = $_POST['typeb'];
+			$typeb = $_POST['typeb'];			
+			$data = $_POST['data'];
 			//wp_get_current_user();
 			//$user = $current_user->user_login;			
 			$table = $wpdb->prefix.'ig_tracker_prd';
 			$query_data = array('user'=>$user,'datetype' => $datetype, 'section' => $section, 'typea' => $typea, 'typeb' => $typeb,'created_date' => $date, 'data'=> $data);
 			$format = array( '%s', '%s', '%s', '%s','%s', '%s', '%s');
-			$wpdb->insert($table,$query_data,$format);
+			$wpdb->insert($table,$query_data,$format);				
 			return 'Success - added';
         } else {
 			
@@ -115,13 +117,50 @@ function output_last_week(){
     $username = $current_user->user_login;
 	$sql = $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}ig_tracker_prd WHERE YEARWEEK(created_date) = YEARWEEK('".$date."') AND user = '".$username."' AND datetype = 'weekly'");
 	$check_array = $wpdb->get_results( $sql , ARRAY_A );
-	if(count($check_array)>0){  ?>
+	
+	// Get the earliest "created_date", which we'll assume was created when the user was
+	// first registered as an alternative means to get the first possible date
+	// $sql = $wpdb->prepare( "SELECT min(created_date) as `created_date` FROM {$wpdb->prefix}ig_tracker_prd WHERE YEARWEEK(created_date) <= YEARWEEK('".$date."') AND user = '".$username."' AND datetype = 'weekly'");
+	// $first_plan_date = $wpdb->get_results( $sql , ARRAY_A );
+	
+	if(count($check_array)>0){ ?>
 
 		<div><a href="https://socialteacup.co.uk/the-gram-plan/?date=<?php echo $date; ?>" ><h3 style="color:#EFECED; font-size: 1.5em;">Previous Weeks 'Gram Plan</h3></a></div>
 
-<?php	} else { ?>
+<?php	    		 
+		// Get user's registration date
+    	$udata = get_userdata( $current_user->ID );
+    	$registered = strtotime($udata->user_registered);
+							  
+		// Determine all Mondays after the user's registration date, but before Monday last week
+		$startMonday = strtotime('last Monday', $registered);
+		$endDate = strtotime('-2 week', strtotime('Monday')); 
+							  
+		echo "<!-- User Reg: " . date('Y-m-d', $registered) . "-->";
+		echo "<!-- First plan: " . date('Y-m-d', $startMonday) . "-->";
+		echo "<!-- Last plan: " . date('Y-m-d', $endDate) . "-->";
+							 					   
+		for ($i = $endDate; $i >= $startMonday; $i = strtotime('-1 week', $i)) {
+			// Output each Monday
+    		$sunday = strtotime('Sunday', $i);
+			?>
+	<div class="elementor-element elementor-element-8303dd9 testfont elementor-widget elementor-widget-heading" data-id="8303dd9" data-element_type="widget" data-widget_type="heading.default">
+		<div class="elementor-widget-container">
+			<h5 class="elementor-heading-title elementor-size-default">
+				<a href="/the-gram-plan/?date=<?php echo date('Y-m-d', $sunday) ?>">
+					The Gram Plan<br>
+					<span style="color:#D0D6F6; font-size:0.8em;"><?php echo date('M', $i) . " - " . date('j', $i) . "-" . date('jS', $sunday) ?></span>
+				</a>
+			</h5>		
+		</div>
+	</div>
+		<?php
+		}
+
+							 } else { ?>
 
 		<div><h4 style="color:#EFECED;">Previous Weeks - Does Not Exist</h4></div>
 
-<?php }
+<?php
+									}
 }
